a2742aa0d239fcb034f9d8aeacf679b5e5e3df22
benxta4424 <benxtaben@gmail.com>
2025-01-07 01:33:11 +0200

    displaying the results of the search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @airports=Airport.all
+    @flights=Flight.all
+
+    @starting_location=Airport.find(params[:current_location]).name
+    @ending_location=Airport.find(params[:desired_location]).name
+    @date=Flight.find(params[:date]).date
   end
 end
