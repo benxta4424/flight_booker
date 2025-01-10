16bbf9e393af13da4e00c64482e8b23d07173f3c
benxta4424 <benxtaben@gmail.com>
2025-01-10 16:36:40 +0200

    fixing the date (problem: i was getting the date id instead of the date in the search form) adding messages for the not found flights and the same city flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,19 +1,33 @@
 class FlightsController < ApplicationController
   def index
-    @airports=Airport.all
-    @flights=Flight.all
+    @airports = Airport.all
+    @flights = Flight.all
 
-    @departure_airport=Airport.find_by(id: params[:current_location])
-    @arrival_airport=Airport.find_by(id: params[:desired_location])
-    @date = Flight.find_by(date: params[:flight_date])&.date
+    @departure_airport = Airport.find_by(id: params[:current_location])
+    @arrival_airport = Airport.find_by(id: params[:desired_location])
+    flight_date = Flight.find_by(date: params[:flight_date])&.date
 
-    @starting_location=@departure_airport&.name
-    @ending_location=@arrival_airport&.name
+    # Log parameters and parsed data
+    Rails.logger.debug "Params Flight Date: #{params[:flight_date]}"
+    Rails.logger.debug "Parsed Flight Date: #{flight_date}"
+    Rails.logger.debug "Departure Airport: #{@departure_airport&.id}"
+    Rails.logger.debug "Arrival Airport: #{@arrival_airport&.id}"
 
+    if @departure_airport == @arrival_airport
+      flash.now[:same_location] = "You can't fly to the same city!"
+      return
+    end
 
-    @found_flights=Flight.where(
+    @found_flights = Flight.where(
+      date: flight_date,
       arrival_airport_id: @arrival_airport&.id,
-      departure_airport_id: @departure_airport&.id,
+      departure_airport_id: @departure_airport&.id
     )
+
+    Rails.logger.debug "Found Flights: #{@found_flights.inspect}"
+
+    if @found_flights.empty?
+      flash.now[:error] = "No flights found!"
+    end
   end
 end
