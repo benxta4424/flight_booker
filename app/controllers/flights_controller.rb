403375bde77d2b8f866aaa0445e03c1918c48332
benxta4424 <benxtaben@gmail.com>
2025-01-10 22:57:57 +0200

    adding a stylesheet for better looking results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,12 +7,6 @@ class FlightsController < ApplicationController
     @arrival_airport = Airport.find_by(id: params[:desired_location])
     flight_date = Flight.find_by(date: params[:flight_date])&.date
 
-    # Log parameters and parsed data
-    Rails.logger.debug "Params Flight Date: #{params[:flight_date]}"
-    Rails.logger.debug "Parsed Flight Date: #{flight_date}"
-    Rails.logger.debug "Departure Airport: #{@departure_airport&.id}"
-    Rails.logger.debug "Arrival Airport: #{@arrival_airport&.id}"
-
     if @departure_airport == @arrival_airport
       flash.now[:same_location] = "You can't fly to the same city!"
       return
@@ -23,8 +17,6 @@ class FlightsController < ApplicationController
       arrival_airport_id: @arrival_airport&.id,
       departure_airport_id: @departure_airport&.id
     )
-
-    Rails.logger.debug "Found Flights: #{@found_flights.inspect}"
 
     if @found_flights.empty?
       flash.now[:error] = "No flights found!"
