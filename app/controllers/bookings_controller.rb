0fb4e8efba5ef308a4cff29860746c7ef82c5850
benxta4424 <benxtaben@gmail.com>
2025-01-11 17:16:56 +0200

    creating the routes for the New/Create for the Bookings(i forgot to add the Index which i'll do later)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,20 @@
 class BookingsController < ApplicationController
   def new
+    @booking=Booking.new(booking_params)
+  end
+
+  def create
+    @booking=Booking.new(booking_params)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:bookings).permit(:flight_id, :passager_id)
   end
 end
