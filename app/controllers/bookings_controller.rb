add5a4b9cef335e33b5baac3cd2ce92e6609b995
benxta4424 <benxtaben@gmail.com>
2025-01-15 01:42:52 +0200

    fixing the bookings controller: if the booking is succesful we redirect the user to all the bookings he/she has fixed incorrectly new route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,20 +4,21 @@ class BookingsController < ApplicationController
   end
 
   def new
-    @booking=Booking.new(flight_id: params[:radio_btn], passager_id: params[:passager_id])
+    @booking=Booking.new
   end
 
   def create
     @booking = Booking.new(flight_id: params[:radio_btn], passager_id: params[:passager_id])
     if @booking.save
-      render :index
+      flash[:success]="Booking succesfully created!"
+      redirect_to bookings_path(@booking.id)
     else
+      flash[:booking_error]="Unsuccessful booking! Try again!"
       redirect_to new_passager_path(flight_id: @booking.flight_id)
     end
   end
-end
-private
 
-def booking_params
-  params.require(:booking).permit(:flight_id, :passager_id)
+  def show
+    @booking = Booking.find(params[:id])
+  end
 end
