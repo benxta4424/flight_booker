36072fbeec5819183f91812516a8cf41a6a153b8
benxta4424 <benxtaben@gmail.com>
2025-01-18 04:37:28 +0200

    creating the views for booking a flight

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,17 +4,33 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @flight=Flight.find_by(id: params[:flight_id])
     @booking=Booking.new
   end
 
   def create
-    @booking = Booking.new(flight_id: params[:radio_btn], passager_id: params[:passager_id])
-    if @booking.save
-      flash[:success]="Booking succesfully created!"
-      redirect_to bookings_path(@booking.id)
+    @flight=Flight.find_by(id: params[:flight_id])
+
+    if params[:name].present? && params[:email].present?
+      # creating the passager
+      flash.now[:success_passager]="You've succesfully created the Passager!"
+      @passager=Passager.create(name: params[:name], email: params[:email])
     else
-      flash[:booking_error]="Unsuccessful booking! Try again!"
-      redirect_to new_passager_path(flight_id: @booking.flight_id)
+      render :new
+      flash.now[:failure_passager]="You've failed to create the Passager!"
+    end
+
+    if @passager.present? && @flight.present?
+      @booking = Booking.new(flight_id: @flight.id, passager_id: @passager.id)
+    end
+
+    if @booking.present?
+      if @booking.save
+        flash[:success]="Booking succesfully created!"
+        redirect_to booking_path(@booking.id)
+      else
+        flash[:booking_error]="Unsuccessful booking! Try again!"
+      end
     end
   end
 
--- app/controllers/passagers_controller.rb
+++ app/controllers/passagers_controller.rb
@@ -4,21 +4,20 @@ class PassagersController < ApplicationController
   end
 
   def new
-    Rails.logger.debug "Params received in new: #{params.inspect}"
-    @flight = Flight.find_by(id: params[:flight_id])
     @passager = Passager.new
   end
-
 
   def create
     @passager=Passager.new(passager_params)
     if @passager.save
-      flash.now[:succesful_creation]="Welcome,#{@passager.name}!"
-      redirect_to passager_path(@passager)
+      flash.now[:successful_creation]="Welcome,#{@passager.name}!"
     else
       flash.now[:creation_error]="Couldn't create the user. Try again!"
-      render :new
     end
+  end
+
+  def show
+    @passager = Passager.find(params[:id])
   end
 
   private
